aceadbfc48a975d931d94fdbb0811f30735906e9
yevgen.martsynkevych <yevgen.martsynkevych@itera.com>
2026-07-03 19:04:26 +0200

    initial project

---
+++ src/main/java/com/ohpen/midoffice/configtracker/ConfigtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ohpen.midoffice.configtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigtrackerApplication.class, args);
+	}
+
+}
